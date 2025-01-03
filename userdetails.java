fda5cb5e6466cf5bd1dfc860b80cbc9659b9d91a
Bhargavi-YadavG <bhargaviyadav6303@gmail.com>
2025-01-03 10:44:27 +0530

    User Details using while loop

---
+++ userdetails.java
@@ -0,0 +1,12 @@
+public class userdetails {
+    public static void main(String[] args) {
+        String username = "bhargavi06";
+        String pwd = "bhargavi_06";
+        while((username == "bhargavi06") && (pwd == "bhargavi_06")) {
+            System.out.println("Hello Bhargavi you are welcome");
+            break;
+        }
+        
+    }
+}
+
