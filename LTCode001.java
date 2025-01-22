a78e0d6a9594fcd50e72e46ce2fb87e9de467456
Bhargavi-YadavG <bhargaviyadav6303@gmail.com>
2025-01-22 12:59:33 +0530

    Palindrome code from Leet Code

---
+++ LTCode001.java
@@ -0,0 +1,24 @@
+public class LTCode001 {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int original = x;
+        int reversed = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+
+    public static void main(String[] args) {
+        LTCode001 solution = new LTCode001();
+        int[] testNumbers = {121, -121, 10, 12321, 0};
+        
+        for (int num : testNumbers) {
+            System.out.println("Is " + num + " a palindrome? " + solution.isPalindrome(num));
+        }
+    }
+}
